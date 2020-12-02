1a96c6b2fb45d2ac55b68c1a25e29c5bcf366530
NicoDVK <71187149+NicoDVKa@users.noreply.github.com>
2020-12-02 15:48:50 -0300

    Comienzo

---
+++ Grafo.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+
+namespace Grafo
+{
+	public class Grafo<T>
+	{
+		public Grafo()
+		{
+		}
+
+		private List<Vertice<T>> vertices = new List<Vertice<T>>();
+
+		private Hashtable visitados = new Hashtable();
+
+		public void agregarVertice(Vertice<T> v)
+		{
+			v.setPosicion(vertices.Count + 1);
+			vertices.Add(v);
+		}
+
+		public void eliminarVertice(Vertice<T> v)
+		{
+			vertices.Remove(v);
+		}
+
+		public void conectar(Vertice<T> origen, Vertice<T> destino, int peso)
+		{
+			origen.getAdyacentes().Add(new Arista<T>(destino, peso));
+		}
+
+		public void desConectar(Vertice<T> origen, Vertice<T> destino)
+		{
+			Arista<T> arista = origen.getAdyacentes().Find(a => a.getDestino().Equals(destino));
+			origen.getAdyacentes().Remove(arista);
+		}
+
+
+		public List<Vertice<T>> getVertices()
+		{
+			return vertices;
+		}
+
+
+		public Vertice<T> vertice(int posicion)
+		{
+			return this.vertices[posicion];
+		}
+
+
+		public void DFS(Vertice<T> origen)
+		{
+
+			if (!this.visitar(origen)) //Verifico que U no este visitado
+			{
+				Console.Write(origen.getDato()+ " "); //Muestro el dato
+
+				List<Arista<T>> vecinos = origen.getAdyacentes(); //Vecinos de U
+
+				foreach (Arista<T> arista in vecinos) //Verifico que U tenga vecinos
+				{
+					this.DFS(arista.getDestino()); //Recursivamente aplicamos DFS
+				}
+			}
+		}
+
+		public void BFS(Vertice<T> origen)
+		{
+			this.visitar(origen); //Visito U
+			Cola<Vertice<T>> cola = new Cola<Vertice<T>>();
+			cola.encolar(origen);//Encolo U
+			while (!cola.esVacia()) {
+				Vertice<T> vertice = cola.desencolar();//Desencolo U
+				Console.Write(vertice.getDato()+" ");//Muestro el dato de U
+				if (vertice.getAdyacentes().Count != 0)//Verifico que tenga vecinos 
+				{
+					List<Arista<T>> vecinos = vertice.getAdyacentes();
+					foreach (Arista<T> arista in vecinos)
+					{
+						if (!this.visitar(arista.getDestino())){
+
+							cola.encolar(arista.getDestino()); //Encolos los vecinos de U que no fueron visitados
+						}
+					}
+				}
+			}
+		}
+
+		public void resetVisitas()
+        {
+			visitados = new Hashtable();
+        }
+		
+
+		public bool visitar(Vertice<T> nodo) {
+			
+			if (visitados.ContainsKey(nodo))
+			{
+				return true; //Si U ya fue visitado
+			}
+			else 
+			{
+				visitados.Add(nodo,true);
+				return false;   //Si U no fue visitado
+			}
+		}
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Grafo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vertice<int> vertice1 = new Vertice<int>(1);
+            Vertice<int> vertice2 = new Vertice<int>(2);
+            Vertice<int> vertice3 = new Vertice<int>(3);
+            Vertice<int> vertice4 = new Vertice<int>(4);
+            Vertice<int> vertice5 = new Vertice<int>(5);
+            Vertice<int> vertice6 = new Vertice<int>(6);
+            Vertice<int> vertice7 = new Vertice<int>(7);
+            Vertice<int> vertice8 = new Vertice<int>(8);
+
+            Grafo<int> grafo = new Grafo<int>();
+
+            grafo.conectar(vertice1,vertice5,0);
+            grafo.conectar(vertice2, vertice1, 0);
+            grafo.conectar(vertice2, vertice3, 0);
+            grafo.conectar(vertice3, vertice7, 0);
+            grafo.conectar(vertice4, vertice1, 0); 
+            grafo.conectar(vertice4, vertice2, 0);
+            grafo.conectar(vertice4, vertice8, 0);
+            grafo.conectar(vertice5, vertice6, 0);
+            grafo.conectar(vertice6, vertice8, 0);
+            grafo.conectar(vertice8, vertice7, 0);
+
+            Console.Write("Recorrido DFS: "); grafo.DFS(vertice4);
+
+            grafo.resetVisitas();   Console.WriteLine();
+
+            Console.Write("Recorrido BFS: ");grafo.BFS(vertice4);
+
+            Console.ReadKey(true);
+        }
+    }
+}
